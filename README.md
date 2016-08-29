5ce5830c604a34f2d559593e0fc0e27aabd41c3d
anbelespinal <anabelespinal>
2016-08-29 08:11:52 -0500

    operaciones logicas

---
+++ README.md
@@ -0,0 +1,18 @@
+*******
+
+OPERACIONES LOGICAS:
+PSEUDOCODIGO.-
+1- inicio
+2- brindar opciones de operación
+3- ¿leer datos:?
+    3.1- opción escogida
+    3.2- opción no válida
+         3.2.a- volver a 2
+4- mostrar resultado
+5- fiN
+
+DIAGRAMA DE FLUJO:
+
+![sin titulo](http://i68.tinypic.com/24w4obo.jpg)
+
+*******
---
+++ main.js
@@ -0,0 +1,86 @@
+var opcion = parseInt(prompt("ESCOGA UNA OPCIÓN:\n"+
+    "1- suma (a+b)\n"+
+    "2- resta (a-b)\n"+
+    "3- multiplicación (a*b)\n"+
+    "4- division (a/b)\n"+
+    "5- resto (a%b)\n"+
+    "6- menor que (a<b)\n"+
+    "7- mayor que (a>b)\n"+
+    "8- menor o igual que (a<=b)\n"+
+    "9- mayor o igual que (a>=b)\n"+
+    ("POR FAVOR INDIQUE EL NÚMERO DE LA OPCIÓN")));
+switch (opcion) {
+     case 1:
+        var numero1 = parseInt(prompt("Inserte el primer número: "));
+        var numero2 = parseInt(prompt("Inserte el segundo número: "));
+        var suma = numero1 + numero2;
+        window.alert("El resultado de la suma es: " + suma);
+    break;
+    case 2:
+        var numero1 = parseInt(prompt("Inserte el primer número: "));
+        var numero2 = parseInt(prompt("Inserte el segundo número: "));
+        var resta = numero1 - numero2;
+        window.alert("El resultado de la resta es: " + resta);
+    break;
+    case 3:
+        var numero1 = parseInt(prompt("Inserte el primer número: "));
+        var numero2 = parseInt(prompt("Inserte el segundo número: "));
+        var multiplicacion = numero1 * numero2;
+        window.alert("El resultado de la multiplicaión es: " + multiplicacion);
+    break;
+     case 4:
+        var numero1 = parseInt(prompt("Inserte el primer número: "));
+        var numero2 = parseInt(prompt("Inserte el segundo número: "));
+        var division = numero1 / numero2;
+        window.alert("El resultado de la división es: " + division);
+    break;
+    case 5:
+        var numero1 = parseInt(prompt("Inserte el primer número: "));
+        var numero2 = parseInt(prompt("Inserte el segundo número: "));
+        var resto = numero1 % numero2;
+        window.alert("El resultado del resto es: " + resto);
+    break;
+    case 6:
+        var numero1 = parseInt(prompt("Inserte el primer número: "));
+        var numero2 = parseInt(prompt("Inserte el segundo número: "));
+        var menorque = numero1 < numero2;
+        if (menorque) {
+            window.alert(numero1 + " MENOR QUE " + numero2);
+        } else {
+            window.alert(numero2 + " MENOR QUE " + numero1);
+        }
+    break;
+    case 7:
+        var numero1 = parseInt(prompt("Inserte el primer número: "));
+        var numero2 = parseInt(prompt("Inserte el segundo número: "));
+        var mayorque = numero1 > numero2;
+        if (mayorque) {
+            window.alert(numero1 + " MAYOR QUE " + numero2);
+        } else {
+            window.alert(numero2 + " MAYOR QUE " + numero1);
+        }
+    break;
+    case 8:
+        var numero1 = parseInt(prompt("Inserte el primer número: "));
+        var numero2 = parseInt(prompt("Inserte el segundo número: "));
+        var menorigualque = numero1 <= numero2;
+        if (menorigualque) {
+            window.alert(numero1 + "<= " + numero2);
+        } else {
+            window.alert(numero2 + " <= " + numero1);
+        }
+    break;
+    case 9:
+        var numero1 = parseInt(prompt("Inserte el primer número: "));
+        var numero2 = parseInt(prompt("Inserte el segundo número: "));
+        var mayorigualque = numero1 >= numero2;
+        if (mayorigualque) {
+            window.alert(numero1 + ">= " + numero2);
+        } else {
+            window.alert(numero2 + " >= " + numero1);
+        }
+    break;
+    default:
+        window.alert("ESTE NÚMERO NO ES UNA OPCIÓN PARA OPERAR.\n"+
+                     "VUELVE A INTENTARLO :)");
+}
